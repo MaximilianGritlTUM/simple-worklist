a5c36497f7bb179e06e07095c50fda1d66644c5a
MaximilianGritlTUM <maximilian.gritl@tum.de>
2024-04-23 19:45:32 +0200

    cleaned up filtering of subjects

--- app.js
+++ app.js
@@ -17,11 +17,49 @@ app.listen(PORT, () => {
 });
 
 app.post("/", (request, response) => {
-    let foundSubjects = []
-    let found = false
+
     let cpee_instance_url = request.headers['cpee-instance-url'];
     let concerns = JSON.parse(('[' + request.body.concerns.slice(2,-2) + ']').replace(new RegExp('"concern": ', 'g'), ''));
-    axios.get(cpee_instance_url + '/properties/description').then(async resp => {
+
+    filterSubjects(cpee_instance_url, concerns).then(subject => {
+        dict[subject.uid] = {}
+        dict[subject.uid]["callback"] = request.headers['cpee-callback']
+        dict[subject.uid]["label"] = request.headers['cpee-label']
+        dict[subject.uid]["activity"] = request.headers['cpee-activity']
+        dict[subject.uid]["instance"] = request.headers['cpee-instance']
+        response.set('CPEE-CALLBACK', 'true')
+        response.send()
+        axios.put(dict[subject.uid]["callback"], {}, {headers: {
+            "CPEE-UPDATE": "true",
+            "CPEE-UPDATE-STATUS" : "assigned task to user "+subject.uid
+        }})
+    });
+});
+
+app.get("/tasks/*", (request, response) => {
+    response.send(dict[request.params[0]])
+})
+
+app.delete("/tasks/*", (request, response) => {
+    if (request.params[0] in dict) {
+        axios.put(dict[request.params[0]]['callback'], {user : request.params[0]})
+        delete dict[request.params[0]];
+        response.send(true);
+    }else{
+        response.send(false);
+    }
+})
+
+app.get("/tasks", (request, response) => {
+    response.send(dict)
+})
+
+function filterSubjects(cpee_instance_url, concerns) {
+    
+    let foundSubjects = []
+    let found = false
+
+    let subject = axios.get(cpee_instance_url + '/properties/description').then(async resp => {
         let concernData = (JSON.parse(parser.toJson(resp.data))).description._concerns.concern;
         for (const concern of concerns) {
             for (const c of concernData) {
@@ -29,19 +67,19 @@ app.post("/", (request, response) => {
                     try {
                         const org = await axios.get(c.orgmodel)
 
-                        if (request.headers['cpee-instance'] in concernsSaved && concern.id in concernsSaved[request.headers['cpee-instance']]) {
+                        if (cpee_instance_url in concernsSaved && concern.id in concernsSaved[cpee_instance_url]) {
                             if(c.type == "BOD") {
-                                if (!found && (foundSubjects.length == 0 || foundSubjects.find(toFind => toFind.uid == concernsSaved[request.headers['cpee-instance']][concern.id]))) {
-                                    foundSubjects = [concernsSaved[request.headers['cpee-instance']][concern.id]]
+                                if (!found && (foundSubjects.length == 0 || foundSubjects.find(toFind => toFind.uid == concernsSaved[cpee_instance_url][concern.id]))) {
+                                    foundSubjects = [concernsSaved[cpee_instance_url][concern.id]]
                                 } else {
                                     foundSubjects = []
                                     found = true
                                 }
                             }else if (c.type == "SOD"){
                                 if (!found && foundSubjects.length == 0) {
-                                    foundSubjects = JSON.parse(parser.toJson(org.data)).organisation.subjects.subject.filter(toFilter => toFilter.uid != concernsSaved[request.headers['cpee-instance']][concern.id].uid)
+                                    foundSubjects = JSON.parse(parser.toJson(org.data)).organisation.subjects.subject.filter(toFilter => toFilter.uid != concernsSaved[cpee_instance_url][concern.id].uid)
                                 }else{
-                                    foundSubjects = foundSubjects.filter(toFilter => toFilter.uid != concernsSaved[request.headers['cpee-instance']][concern.id])
+                                    foundSubjects = foundSubjects.filter(toFilter => toFilter.uid != concernsSaved[cpee_instance_url][concern.id])
                                 }
                                 if (foundSubjects.length == 0) {
                                     found = true
@@ -87,48 +125,18 @@ app.post("/", (request, response) => {
             for (const subject of foundSubjects) {
                 if (!(subject.uid in dict)) {
                     for (const concern of concerns) {
-                        if (!(request.headers['cpee-instance'] in concernsSaved)) {
-                            concernsSaved[request.headers['cpee-instance']] = {}
+                        if (!(cpee_instance_url in concernsSaved)) {
+                            concernsSaved[cpee_instance_url] = {}
                         }
-                        if (!(concern.id in concernsSaved[request.headers['cpee-instance']])) {
-                            concernsSaved[request.headers['cpee-instance']][concern.id] = subject
+                        if (!(concern.id in concernsSaved[cpee_instance_url])) {
+                            concernsSaved[cpee_instance_url][concern.id] = subject
                         }
                     }
-                    dict[subject.uid] = {}
-                    dict[subject.uid]["callback"] = request.headers['cpee-callback']
-                    dict[subject.uid]["label"] = request.headers['cpee-label']
-                    dict[subject.uid]["activity"] = request.headers['cpee-activity']
-                    dict[subject.uid]["instance"] = request.headers['cpee-instance']
-                    response.set('CPEE-CALLBACK', 'true')
-                    response.send('test')
-                    found = true;
-                    axios.put(dict[subject.uid]["callback"], {}, {headers: {
-                        "CPEE-UPDATE": "true",
-                        "CPEE-UPDATE-STATUS" : "assigned task to user "+subject.uid
-                    }})
-                    return
+                    return subject
                 }
             }
-        } else {
-            response.status(404).send("Subject could not be found!");
         }
     })
-});
 
-app.get("/tasks/*", (request, response) => {
-    response.send(dict[request.params[0]])
-})
-
-app.delete("/tasks/*", (request, response) => {
-    if (request.params[0] in dict) {
-        axios.put(dict[request.params[0]]['callback'], {user : request.params[0]})
-        delete dict[request.params[0]];
-        response.send(true);
-    }else{
-        response.send(false);
-    }
-})
-
-app.get("/tasks", (request, response) => {
-    response.send(dict)
-})
+    return subject;
+}
